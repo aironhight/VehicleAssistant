5342208da88b1af13348a4f242156018ab0ce115
aironhight <aironhight@yahoo.com>
2019-10-07 19:09:45 +0200

    Test

--- app/src/main/java/com/aironhight/vehicleassistant/Activity/main/MainActivity.java
+++ app/src/main/java/com/aironhight/vehicleassistant/Activity/main/MainActivity.java
@@ -53,7 +53,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //test
         firebaseAuth = FirebaseAuth.getInstance();
         user =  FirebaseAuth.getInstance().getCurrentUser();
 
